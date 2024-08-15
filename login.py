526c9b65a4994b9a03041fbe87210f7b03fdd555
Shivam Krishna <samverma155@gmail.com>
2024-08-15 11:18:08 +0530

    first commit

---
+++ login.py
@@ -0,0 +1,11 @@
+# from tkinter import*
+# from tkinter import ttk
+# from PIL import Image, ImageTk
+# # import randoom 
+# import time
+# import datetime
+# import mysql.connector
+
+# from main import  Face_Recognition_System
+
+ 
